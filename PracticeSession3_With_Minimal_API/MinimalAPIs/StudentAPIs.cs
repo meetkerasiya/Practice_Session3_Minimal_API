d00e6fab15923c183527e32ce46cdd9f34210601
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-03-07 17:24:51 +0530

    Post method added

--- PracticeSession3_With_Minimal_API/MinimalAPIs/StudentAPIs.cs
+++ PracticeSession3_With_Minimal_API/MinimalAPIs/StudentAPIs.cs
@@ -11,6 +11,8 @@ namespace PracticeSession3_With_Minimal_API.MinimalAPIs
         public static void MapRoutes(WebApplication app)
         {
             app.MapGet("/students", GetAllStudents);
+            app.MapGet("/students/{id}", GetStudent).WithName("Get");
+            app.MapPost("/students", AddStudent);
         }
 
         private static async  Task<IResult>  GetAllStudents([FromServices] IDataRepository<Student> dataRepository)
@@ -25,6 +27,50 @@ namespace PracticeSession3_With_Minimal_API.MinimalAPIs
                 return TypedResults.Problem($"Internal server error: {ex}");
             }
             
+        }
+
+        private static async Task<IResult> GetStudent(int id,[FromServices] IDataRepository<Student> dataRepository)
+        {
+            try
+            {
+                Student student= dataRepository.Get(id);
+                if (student == null)
+                {
+                    return TypedResults.NotFound("This student record could not be found");
+                }
+                return TypedResults.Ok(student);
+            }
+            catch (Exception ex)
+            {
+                return TypedResults.Problem($"Internal server error: {ex}");
+            }
+        }
+
+        private static async Task<IResult> AddStudent([FromBody] StudentDto studentdto, [FromServices] IDataRepository<Student> dataRepository)
+        {
+            try
+            {
+                if(studentdto==null)
+                {
+                    return TypedResults.BadRequest("Student is null");
+                }
+                var student = new Student()
+                {
+                    FirstName = studentdto.FirstName,
+                    LastName = studentdto.LastName,
+                    City = studentdto.City,
+                };
+
+                dataRepository.Add(student);
+
+                return TypedResults.CreatedAtRoute(routeName:"Get",
+                    routeValues: new { Id = student.StudentID },
+                    value: student);
+            }
+            catch (Exception ex)
+            {
+                return TypedResults.Problem($"Internal server error: {ex}");
+            }
         }
        
     }
